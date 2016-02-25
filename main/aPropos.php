71bff7bf6a87fc9ecfbf81d36a7c5b4b55d58cf6
virust11 <samuelprezeau@hotmail.fr>
2016-02-25 15:45:18 +0100

    ajout page a propos
    pas eu le temps de remplir encore..

---
+++ main/aPropos.php
@@ -0,0 +1,21 @@
+<?php include('header.php')?>
+
+<!-- Intégration du css -->
+    <link href="../src/css/dashboard.css" rel="stylesheet">
+
+<!-- Corps -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            
+            
+            
+        </div>
+    </div>
+</div>
+
+<!-- Intégration du js -->
+    <script src="../src/javascript/jquery.js"></script>
+    <script src="../src/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+
+<?php include('footer.php') ?>
